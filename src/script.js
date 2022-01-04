611fde104b4b74aaf8978619525b3a91d051a3af
MacDada <contact@dnowak.pl>
2022-01-04 12:37:25 +0100

    extracted init.js

--- src/script.js
+++ src/script.js
@@ -1,33 +1,5 @@
 'use strict';
 
-$('ul.mainnav').append((function () {
-    const isDisabled = localStorage['DnWH_disabled'];
-
-    const $button = $('<a href="#"></a>')
-        .text(isDisabled ? 'Włącz DnWykopHider' : 'Wyłącz DnWykopHider')
-        .click(function (event) {
-            event.preventDefault();
-
-            if (isDisabled) {
-                localStorage.removeItem('DnWH_disabled');
-            } else {
-                localStorage['DnWH_disabled'] = 'true';
-            }
-
-            window.location.reload();
-        });
-
-    if (!isDisabled) {
-        runWykopHider({
-            hiddenOpacity: 0.2,
-            visibleFirst: true,
-            gcDays: 10
-        });
-    }
-
-    return $('<li></li>').append($button);
-})());
-
 function runWykopHider(options) {
     const hiddenClass = 'DnWH_hidden';
 
